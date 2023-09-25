c46d760a31a67c641eb21b9af9456b8ec65dd1c4
JosieRice <josiericetong@gmail.com>
2023-09-25 16:25:41 -0700

    added db update when marking things as complete or not

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 interface Props {
   id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
 }
 
-export function TodoItem({ id, title, complete }: Props) {
+export function TodoItem({ id, title, complete, toggleTodo }: Props) {
   return (
     <li className="flex gap-1 items-center">
-      <input id={id} type="checkbox" className="cursor-pointer peer"></input>
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label
         htmlFor={id}
         className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
